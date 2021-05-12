e6ba767c60062ec529322165e1df8cdcdbe5fd53
Wilberforce Zachary Ouma <wilalebit@yahoo.com>
2021-05-12 09:52:21 -0400

    Add files via upload

--- 2_gpuBatchScript.sh
+++ 2_gpuBatchScript.sh
@@ -2,16 +2,21 @@
 #SBATCH --time=00:05:00
 #SBATCH --nodes=1 --ntasks-per-node=14  --gpus-per-node=1
 #SBATCH --job-name=partialGPUjob
-#SBATCH --account=PZS0712
+#SBATCH --account=PZS1010
 
 
 cd $SLURM_SUBMIT_DIR
 
 #Load cuda module for Nvidia libraries
 module load cuda
+module load R/3.6.1-gnu9.1
 
 #output goes to the slurm job output file
 echo -e "This is a partial node GPU job:"
 scontrol show node $SLURMD_NODENAME
+
+
+#Run an R script for matrix multiplication
+Rscript  gpuCode.R
 
 sleep 300
